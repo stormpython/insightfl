7b33f584ad070b738dc7d732c95b6d793e425e6b
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-03-14 13:55:14 -0700

    adding first lines of code

---
+++ webserver.py
@@ -0,0 +1,22 @@
+# Imports
+import mysqldb
+from flask import Flask, request, redirect, url_for, \
+    render_template
+
+# Create our application
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run()
