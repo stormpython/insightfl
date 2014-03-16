967ccb0d74f5a76997d352582ef16b0c2715cc15
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-03-15 18:31:34 -0700

    added 404.html, using url_for to include static files, added a route to 404.html

--- webserver.py
+++ webserver.py
@@ -18,5 +18,9 @@ def about():
 def contact():
     return render_template('contact.html')
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run()
