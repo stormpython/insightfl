2aaa0803fcebe713dfd9a84cdecc49e84971caae
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-03-17 17:01:21 -0700

    adding deployment script for Ubuntu 12.04 server, work in progress

---
+++ deployment/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Deployment Script for deploying to Ubuntu 12.04 Server
+
+sudo apt-get update
+sudo apt-get install python-pip mysql-server -y
+sudo apt-get build-dep python-mysqldb
+
+sudo pip install pip --upgrade
+sudo pip install virtualenv
+
+sudo apt-get install curl make gcc g++
+echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
+mkdir ~/.local/
+mkdir ~/node-latest-install
+cd ~/node-latest-install
+curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
+./configure --prefix=~/local
+make install
+curl https://www.npmjs.org/install.sh | shift
+
+sudo apt-get build-deps git-core
+sudo apt-get install git-core
