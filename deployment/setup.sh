42311d538944371ebaf7a66d745a1a40d9e9e5ed
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-04-10 06:29:08 -0700

    updated

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -5,20 +5,14 @@
 # Installing git and git dependencies, pip, and curl
 sudo apt-get update
 sudo apt-get build-dep git-core -y
-sudo apt-get install git-core python-pip curl -y
+sudo apt-get install git-core python-pip curl nginx -y
 sudo pip install pip --upgrade
 
-# Installing node, npm, and bower
-echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
-mkdir ~/.local/
-mkdir ~/node-latest-install
-cd ~/node-latest-install
-curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
-./configure --prefix=~/local
-make install
-curl https://www.npmjs.org/install.sh | sh
-npm install -g bower
+# Installing node and npm
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
 
 # Change into home directory
 cd ~
