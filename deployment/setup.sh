abb9214aaa1829da4c8354931a1e1508b6904080
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-04-12 17:57:19 -0700

    removing pip install of MySQLdb, too many potential problems

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -2,13 +2,11 @@
 
 # Deployment Script for deploying to Ubuntu 12.04 Server
 
-# Installing git, git dependencies, pip, curl, nginx, and mysql
+# Installing git, git dependencies, pip, curl, nginx, MySQL, and MySQLdb
 sudo apt-get update
 sudo apt-get build-dep git-core -y
-sudo apt-get install git-core python-pip curl nginx mysql-server -y
-sudo apt-get build-dep python-mysqldb -y
+sudo apt-get install git-core python-pip curl nginx mysql-server python-mysqldb -y
 sudo pip install pip --upgrade
-sudo pip install MySQL-python
 
 # Installing node and npm
 sudo apt-get install python-software-properties python g++ make -y
