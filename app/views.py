a45b994fe59ec37a0570f4bbf6b3a224683f4ef8
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-05-06 23:32:24 -0700

    renaming files to conventional names, adding mail server settings

--- app/views.py
+++ app/views.py
@@ -42,3 +42,8 @@ def contact():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
