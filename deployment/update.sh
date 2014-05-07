c3ec1657ca4d50a833f60d635fe29105e42e71c4
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-05-07 00:14:39 -0700

    updating update script

--- deployment/update.sh
+++ deployment/update.sh
@@ -2,6 +2,14 @@
 
 # Update script
 cd /home/ubuntu/insightfl
+
+# Pulls down the latest changes
 git pull origin master
+
+# Installs project dependencies
 npm install
 sudo pip install -r requirements.txt
+
+# terminates the running screen and restarts
+screen -X quit
+screen -d -m python production.py
