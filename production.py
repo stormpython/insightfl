809c8a0b96117d8b8b29407b6e2f0d46fca98661
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-05-06 23:38:46 -0700

    moving error emails to production.py

--- production.py
+++ production.py
@@ -1,6 +1,16 @@
 from app import app
+import logging
+from logging.handlers import SMTPHandler
 
 # Production configuration settings
 app.config.from_pyfile('settings/production.cfg')
+
+# Error emails
+admins = app.config["ADMINS"]
+mail_handler = SMTPHandler('127.0.0.1',
+                           'server-error@example.com',
+                           admins, 'YourApplication Failed')
+mail_handler.setLevel(logging.ERROR)
+app.logger.addHandler(mail_handler)
 
 app.run(debug=False)
