fa163173607117f12584d3b0501f2fdbed9ab28b
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-08-26 16:03:04 +0300

    adding an errors folder in the template directory, correcting css with 404.html

--- app/errors.py
+++ app/errors.py
@@ -4,9 +4,9 @@ from app import app
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_error(error):
-    return render_template('500.html'), 500
+    return render_template('errors/500.html'), 500
