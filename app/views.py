8365d7180408c55e7ec3e7947b52b411e00c9268
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-09-02 16:23:52 +0300

    removed revealjs as presentation library, fixed issues with genericon styling

--- app/views.py
+++ app/views.py
@@ -9,13 +9,6 @@ def index():
     # Renders index.html.
     return render_template('index.html')
 
-
-@app.route('/presentation')
-def about():
-    # Renders presentation.html.
-    return render_template('presentation.html')
-
-
 @app.route('/author')
 def contact():
     # Renders author.html.
