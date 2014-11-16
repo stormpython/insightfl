890f2b6271bb3f65330bcef8788b4a88f75bcff2
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-11-16 11:15:22 +0200

    adding comments

--- config.py
+++ config.py
@@ -3,7 +3,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    # Flask application key
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+
+    # MySQL database settings
     MYSQL_DATABASE_HOST = 'localhost'
     MYSQL_DATABASE_PORT = 3306
     MYSQL_DATABASE_USER = os.environ.get('MYSQL_DATABASE_USER') or None
